478835a17364191f493d85c4ccafa51ac9912b35
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-01-07 15:18:43 -0800

    Fix test

--- internal/vfs/iofs_test.go
+++ internal/vfs/iofs_test.go
@@ -130,6 +130,6 @@ func TestIOFS(t *testing.T) {
 	t.Run("CaseSensitivity", func(t *testing.T) {
 		t.Parallel()
 
-		assert.Assert(t, fs.CaseSensitivity())
+		assert.Assert(t, fs.CaseSensitivity().IsCaseSensitive())
 	})
 }
