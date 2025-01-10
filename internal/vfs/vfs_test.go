674c791779b7e5bb165092072fe5659dacff8f69
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-01-10 11:56:18 -0800

    Benchmark

--- internal/vfs/vfs_test.go
+++ internal/vfs/vfs_test.go
@@ -6,6 +6,8 @@ import (
 	"testing/fstest"
 	"unicode/utf16"
 
+	"github.com/microsoft/typescript-go/internal/repo"
+	"github.com/microsoft/typescript-go/internal/tspath"
 	"github.com/microsoft/typescript-go/internal/vfs"
 	"github.com/microsoft/typescript-go/internal/vfs/vfstest"
 	"gotest.tools/v3/assert"
@@ -166,4 +168,56 @@ func TestBOM(t *testing.T) {
 		assert.Assert(t, ok)
 		assert.Equal(t, content, expected)
 	})
+}
+
+func BenchmarkReadFile(b *testing.B) {
+	type bench struct {
+		name string
+		fs   vfs.FS
+		path string
+	}
+
+	osFS := vfs.FromOS()
+
+	const smallData = "hello, world"
+	tmpdir := tspath.NormalizeSlashes(b.TempDir())
+	osSmallDataPath := tspath.CombinePaths(tmpdir, "foo.ts")
+	err := osFS.WriteFile(osSmallDataPath, smallData, false)
+	assert.NilError(b, err)
+
+	tests := []bench{
+		{"MapFS small", vfstest.FromMapFS(fstest.MapFS{
+			"foo.ts": &fstest.MapFile{
+				Data: []byte(smallData),
+			},
+		}, true), "/foo.ts"},
+		{"OS small", osFS, osSmallDataPath},
+	}
+
+	if repo.TypeScriptSubmoduleExists() {
+		checkerPath := tspath.CombinePaths(tspath.NormalizeSlashes(repo.TypeScriptSubmodulePath), "src", "compiler", "checker.ts")
+
+		checkerContents, ok := osFS.ReadFile(checkerPath)
+		assert.Assert(b, ok)
+
+		tests = append(tests, bench{
+			"MapFS checker.ts",
+			vfstest.FromMapFS(fstest.MapFS{
+				"checker.ts": &fstest.MapFile{
+					Data: []byte(checkerContents),
+				},
+			}, true),
+			"/checker.ts",
+		})
+		tests = append(tests, bench{"OS checker.ts", osFS, checkerPath})
+	}
+
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			b.ReportAllocs()
+			for range b.N {
+				_, _ = tt.fs.ReadFile(tt.path)
+			}
+		})
+	}
 }
