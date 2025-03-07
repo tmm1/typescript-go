3845d9176c399b8ebd52d830844779415d3c089e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-03-06 17:14:51 -0800

    Add check benchmarks

--- internal/checker/checker_test.go
+++ internal/checker/checker_test.go
@@ -1,12 +1,14 @@
 package checker_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/microsoft/typescript-go/internal/ast"
 	"github.com/microsoft/typescript-go/internal/bundled"
 	"github.com/microsoft/typescript-go/internal/checker"
 	"github.com/microsoft/typescript-go/internal/compiler"
+	"github.com/microsoft/typescript-go/internal/core"
 	"github.com/microsoft/typescript-go/internal/repo"
 	"github.com/microsoft/typescript-go/internal/tspath"
 	"github.com/microsoft/typescript-go/internal/vfs/osvfs"
@@ -86,9 +88,52 @@ func BenchmarkNewChecker(b *testing.B) {
 	p := compiler.NewProgram(opts)
 
 	b.ReportAllocs()
-	b.ResetTimer()
 
-	for range b.N {
+	for b.Loop() {
 		checker.NewChecker(p)
+	}
+}
+
+func BenchmarkCheckSrcCompiler(b *testing.B) {
+	repo.SkipIfNoTypeScriptSubmodule(b)
+	fs := osvfs.FS()
+	fs = bundled.WrapFS(fs)
+
+	rootPath := tspath.CombinePaths(tspath.NormalizeSlashes(repo.TypeScriptSubmodulePath), "src", "compiler")
+
+	host := compiler.NewCompilerHost(nil, rootPath, fs, bundled.LibPath())
+	opts := compiler.ProgramOptions{
+		Host:           host,
+		ConfigFileName: tspath.CombinePaths(rootPath, "tsconfig.json"),
+	}
+	p := compiler.NewProgram(opts)
+	p.BindSourceFiles()
+
+	files := p.SourceFiles()
+	findFile := func(name string) *ast.SourceFile {
+		return core.Find(files, func(f *ast.SourceFile) bool {
+			return strings.HasSuffix(f.FileName(), name)
+		})
+	}
+
+	tests := []string{
+		"/compiler/checker.ts",
+		"/compiler/core.ts",
+		"/compiler/program.ts",
+		"/compiler/sys.ts",
+		"/compiler/types.ts",
+	}
+
+	for _, test := range tests {
+		b.Run(tspath.GetBaseFileName(test), func(b *testing.B) {
+			file := findFile(test)
+
+			b.ReportAllocs()
+
+			for b.Loop() {
+				c := checker.NewChecker(p)
+				c.CheckSourceFile(file)
+			}
+		})
 	}
 }
