14a86c9fc63d0d8000940bb0941eab6d0743f5b0
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-03-22 17:06:02 -0700

    Cache MkdirAll for basline outputs

--- internal/testutil/baseline/baseline.go
+++ internal/testutil/baseline/baseline.go
@@ -146,12 +146,25 @@ func RunAgainstSubmodule(t *testing.T, fileName string, actual string, opts Opti
 	writeComparison(t, actual, local, reference, true)
 }
 
+var mkdirAllCache sync.Map
+
+func mkdirAllCached(t *testing.T, path string) error {
+	cached, ok := mkdirAllCache.Load(path)
+	if ok {
+		return cached.(error)
+	}
+
+	err := os.MkdirAll(path, 0o755)
+	cached, _ = mkdirAllCache.LoadOrStore(path, err)
+	return cached.(error)
+}
+
 func writeComparison(t *testing.T, actualContent string, local, reference string, comparingAgainstSubmodule bool) {
 	if actualContent == "" {
 		panic("the generated content was \"\". Return 'baseline.NoContent' if no baselining is required.")
 	}
 
-	if err := os.MkdirAll(filepath.Dir(local), 0o755); err != nil {
+	if err := mkdirAllCached(t, filepath.Dir(local)); err != nil {
 		t.Error(fmt.Errorf("failed to create directories for the local baseline file %s: %w", local, err))
 		return
 	}
